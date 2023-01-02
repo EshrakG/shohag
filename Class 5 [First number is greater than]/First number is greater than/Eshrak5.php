ba2f266bbb497328f86cf9396d3a36080ee8d0b6
EshrakGe <eshrakg62@gmail.comm>
2023-01-03 03:01:44 +0600

    changes css

--- Class 5 [First number is greater than]/First number is greater than/Eshrak5.php
+++ Class 5 [First number is greater than]/First number is greater than/Eshrak5.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Eshrak's নামতা project </title>
+    <title>Playing with numbers project </title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
