a80ae44968f0e69f7d58c74d1a583f893df6cfe4
Eshrak <108010276+EshrakG@users.noreply.github.com>
2023-01-03 08:54:57 +0600

    added comment

--- Class 5 [First number is greater than]/First number is greater than/Eshrak5.php
+++ Class 5 [First number is greater than]/First number is greater than/Eshrak5.php
@@ -9,28 +9,28 @@
 </head>
 <body>
  
-    <div class="container">
-          <div class="card">
-              <div class="inner-box" id="card">
-                <div class="card-front">
-                    <h2>Put your favourite number</h2>
-                    <form name="frm" method="post">
-                        <input type="number" name="f" class="input-box"
-                        placeholder="input here" required>
-
-                        <input type="number" name="s" class="input-box"
-                        placeholder="input here" required>
-
-                        <input type="number" name="t" class="input-box"
-                        placeholder="input here" required>
-
-                        <input type="submit" name="sub" class="input-box">
-                        <br>
-                        <br>
-                          
-
+<div class="container">
+  <div class="card">
+     <div class="inner-box" id="card">
+        <div class="card-front">
+            <h2>Put your favourite number</h2>
+                <form name="frm" method="post">
+                   <input type="number" name="f" class="input-box"
+                              placeholder="input here" required>
+                    <input type="number" name="s" class="input-box"
+                              placeholder="input here" required>
+                    <input type="number" name="t" class="input-box"
+                              placeholder="input here" required>
+                    <input type="submit" name="sub" class="input-box">
+                    <br>
+                <br>
+            </div>
+        </div>
+    </div>
+</div>                                                                
+                    
     <?php
-
+                            
      $first  = @$x = $_POST['f'];
      $second = @$x = $_POST['s'];
      $third  = @$x = $_POST['t'];
@@ -38,28 +38,35 @@
      @$sub   = $_POST['sub'];
 
     if($first > $second && $first > $third  ){
-        echo "<h2> First number is greater than any other numbers </h2>";
+        echo "<h2> First number is greater than any other numbers 😃</h2>";
     }
     elseif($second > $first && $second > $third ){
-        echo "<h2> Second number is greater than all other numbers </h2>";
+        echo "<h2> Second number is greater than all other numbers 😃</h2>";
     }
     elseif($third > $first && $third > $second){
-        echo "<h2> Third number is greater than many other numbers </h2>";
+        echo "<h2> Third number is greater than many other numbers 😃</h2>";
     }
     elseif($third == $first && $third == $second){
-        echo "<h1> All numbers are same!!! </h1>";   
+        echo "<h1> All numbers are same!!! 😅 </h1>";   
     }
     elseif($first == $second ){
-        echo "<h1> First & second  numbers are same!!! </h1>";   
+        echo "<h1> First  & Second numbers are same!!! 😎 </h1>";   
     }
     elseif($third == $second ){
-        echo "<h1> Second & Third  numbers are same!!! </h1>";   
+        echo "<h1> Second & Third numbers are same!!! 😎 </h1>";   
     }
   
     elseif($third == $first ){
-        echo "<h1> First & third  numbers are same!!! </h1>";   
+        echo "<h1> First  & Third numbers are same!!! 😎 </h1>";   
     }  
-            
+ /*           
+ In get method :
+    Restrictions on data length	Yes, when sending data, the GET method adds the data to the URL; and the length of a URL is limited (maximum URL length is 2048 characters)	No restrictions !!!
+
+    In post method :
+    Restrictions on data type Only ASCII characters allowed	No restrictions. Binary data is also allowed !!!
+*/
+
  ?>
 </body>
 </html>
